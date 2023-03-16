e3d737bec73d72f39c915416608bc0f418806444
Thapelo-Sean <thapelomatji12@gmail.com>
2023-03-16 13:02:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {	
-	fwrite(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 2);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
