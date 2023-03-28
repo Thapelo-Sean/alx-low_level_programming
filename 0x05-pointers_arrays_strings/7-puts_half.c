7a9847495f8ceabf7ead6fbda2e66a28f918faf4
Thapelo-Sean <thapelomatji12@gmail.com>
2023-03-28 12:01:12 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int x, y, z;
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+
+	if (x % 2 == 0)
+	{
+		for (z = x % 2; str[z] != '\0'; z++)
+		{
+			_putchar(str[z]);
+		}
+	} else if (x / 2)
+	{
+		for (y = (x - 1) / 2; y < x - 1; y++)
+		{
+			_putchar(str[y + 1]);
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	x = 0;
+
+	while (src[x] != '\0')
+	{
+		x++;
+	}
+
+	for (y = 0; y < x; y++)
+	{
+		dest[y] = src[x];
+	}
+	dest[y] = '\0';
+
+	return (dest);
+}
