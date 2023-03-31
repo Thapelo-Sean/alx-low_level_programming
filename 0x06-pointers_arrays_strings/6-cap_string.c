f5c71410eb08768e5d4511c315aed5e8026b428f
Thapelo-Sean <thapelomatji12@gmail.com>
2023-03-31 04:51:20 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int x, y;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (x == 0& s[x] >= 'a' && s[i] <= 'z')
+			s[x] -= 32;
+
+		for (j = 0; y < 13;yj++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
